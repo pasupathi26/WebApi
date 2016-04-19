f8231a9376df17c47f7b8b7be4c931f9844b0165
Pasupathi Ganesan <Pasupathi Ganesan>
2016-04-20 00:57:43 +0530

    My First WebAPI

---
+++ SampleWebApi/SampleWebApi/Controllers/EmployeeDetailsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SampleWebApi.Models;
+
+namespace SampleWebApi.Controllers
+{
+    public class EmployeeDetailsController : ApiController
+    {
+
+        public IEnumerable<Employee> Get()
+        {
+            var s = new List<Employee>(){
+                
+            new Employee()
+            {
+                UserId = "Emp120",
+                UserName = "Pasupathi",
+                Password = "rabiksha   ",
+                Mobilenumber = "7411182882",
+                Email = "pasupathi26@gmail.com"
+            },
+            new Employee()
+            {
+                UserId = "Emp123",
+                UserName = "Sonikumari",
+                Password = "rabik",
+                Mobilenumber = "9916757680",
+                Email = "soni2mail.xcom"
+            }
+            };
+            return s;
+        }
+        public Employee GetEmployee(int UserID)
+        {
+            var s = new Employee()
+            {
+
+                UserId = "Emp120",
+                UserName = "Pasupathi",
+                Password = "rabiksha   ",
+                Mobilenumber = "7411182882",
+                Email = "pasupathi26@gmail.com"
+
+            };
+            return s;
+        }
+    }
+
+}
---
+++ SampleWebApi/SampleWebApi/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleWebApi.Models
+{
+    public class Employee
+    {
+        public string UserId { set; get; }
+        public string UserName { set; get; }
+        public string Password { set; get; }
+        public string Mobilenumber { set; get; }
+        public string Email { set; get; }
+    }
+}
